2c21920ee4b9aad4711de9b7eb4632c6dd9e0de8
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-25 14:17:38 -0500

    Add duplicate checker

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #define BAG_TYPE int
 #include "bag.h"
+bool check_dups(int *arr,int size)
+{
+	for (int i=0;i<size-1;i++)
+		for (int j=i+1;j<size;j++)
+			if (arr[i]==arr[j])
+				return true;
+	return false;
+}
 int main(int argc,char **argv)
 { // 6 numbers 1-54, no repeats
 	srand(time(NULL));
@@ -17,8 +26,13 @@ int main(int argc,char **argv)
 			printf("%d ",arr[i]);
 			place(b,arr[i]);
 		}
+		if (check_dups(arr,6))
+			printf("(contains duplicates)");
 		putchar('\n');
 	}
+	for (int i=0;i<54;i++)
+		printf("%d ",b->cont[i]);
+	putchar('\n');
 	free_bag(b);
 	return 0;
 }
