0048e62be89f3554ac1454b56f501220578b462b
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-25 13:30:48 -0500

    Fixes

--- bag.h
+++ bag.h
@@ -5,26 +5,39 @@
 #define BAG_TYPE void *
 #endif
 typedef struct {
-	int size,items;
+	int max,items;
 	BAG_TYPE *cont;
 } bag_t;
 bag_t *new_bag(int size)
 {
+	if (!size)
+		return NULL;
 	bag_t *b=malloc(sizeof(bag_t));
-	b->size=size*sizeof(BAG_TYPE);
+	b->max=size;
 	b->items=0;
-	b->cont=malloc(b->size);
+	b->cont=malloc(b->max*sizeof(BAG_TYPE));
 	return b;
+}
+void free_bag(bag_t *b)
+{
+	free(b->cont);
+	free(b);
 }
 BAG_TYPE pick(bag_t *b)
 {
+	if (!b->items)
+		return (BAG_TYPE)0;
 	int n=rand()%b->items;
 	BAG_TYPE i=b->cont[n];
 	b->cont[n]=b->cont[--b->items];
 	return i;
 }
-void put(bag_t *b,BAG_TYPE i)
+void place(bag_t *b,BAG_TYPE i)
 {
+	if (b->items==b->max) {
+		b->max*=2;
+		b->cont=realloc(b->cont,b->max*sizeof(BAG_TYPE));
+	}
 	b->cont[b->items++]=i;
 }
 #endif
--- main.c
+++ main.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 #define BAG_TYPE int
-#include "src/bag.h"
+#include "bag.h"
 int main(int argc,char **argv)
 { // 6 numbers 1-54, no repeats
 	srand(time(NULL));
 	bag_t *b=new_bag(54);
 	for (int i=1;i<=54;i++)
-		put(b,i);
+		place(b,i);
 	for (int i=0;i<100;i++) {
 		int arr[6];
 		for (int i=0;i<6;i++)
 			arr[i]=pick(b);
 		for (int i=0;i<6;i++) {
 			printf("%d ",arr[i]);
-			put(b,arr[i]);
+			place(b,arr[i]);
 		}
 		putchar('\n');
 	}
-	free(b->cont);
-	free(b);
+	free_bag(b);
 	return 0;
 }
